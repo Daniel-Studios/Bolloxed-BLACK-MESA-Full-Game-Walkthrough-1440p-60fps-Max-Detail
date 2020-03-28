9bf031c7a76f445ffdb181fd66038fd4766b0d99
Daniel2019Creator1 <57808482+Daniel2019Creator1@users.noreply.github.com>
2020-03-27 20:56:05 -0400

    script.js
    JS added new.

---
+++ script.js
@@ -0,0 +1,13 @@
+function iframe() {
+document.getElementById("iframe"); {
+    var iframe=("iframe");
+    var onload=("onload");
+    window.onload,iframe=('https://www.youtube.com/watch?v=KHKUVdb5YTM&amp;index=2&amp;t=0s'); {
+        document.body.iframe.href=('https://www.youtube.com/watch?v=KHKUVdb5YTM&amp;index=2&amp;t=0s'); {
+            body.iframe.href=('https://www.youtube.com/watch?v=KHKUVdb5YTM&amp;index=2&amp;t=0s');
+        }
+        document.body.iframe.src=('https://www.youtube.com/embed/KHKUVdb5YTM'); {
+            body.iframe.src=('https://www.youtube.com/embed/KHKUVdb5YTM');
+        }
+    }
+}}
